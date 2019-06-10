ce0206fd2ca6e6b05017be3cdd64190dc7bb9a1b
Alexis Josephides <ajosephides@gmail.com>
2019-06-10 15:26:39 +0100

    added more routes and added shotgun into gemfile

--- app.rb
+++ app.rb
@@ -3,3 +3,15 @@ require 'sinatra'
 get '/' do
   "Hello!"
 end
+
+get '/secret' do
+  "Shhhhhh. I ain't telling you"
+end
+
+get '/shotgun' do
+  "ouch"
+end
+
+get '/welcome' do
+  "please put your feet up"
+end
